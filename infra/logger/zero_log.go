effcccf7cd1cf00c34a27956049eb0c88a0a6415
jinjianfeng <jinjianfeng@yxt.com>
2023-05-12 14:02:22 +0800

    修改时间格式

--- infra/logger/zero_log.go
+++ infra/logger/zero_log.go
@@ -1,16 +1,32 @@
 package logger
 
 import (
+	"fmt"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"os"
 	"strings"
+	"time"
 )
 
 var logger zerolog.Logger
 
 func init() {
-	logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout}).With().Timestamp().Logger()
+	//consoleWriter := zerolog.ConsoleWriter{Out: os.Stdout}
+
+	consoleWriter := zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339}
+	//consoleWriter.FormatLevel = func(i interface{}) string { return strings.ToUpper(fmt.Sprintf("| %-6s|", i)) }
+	//consoleWriter.FormatMessage = func(i interface{}) string { return fmt.Sprintf("***%s****", i) }
+	//consoleWriter.FormatFieldName = func(i interface{}) string { return fmt.Sprintf("%s:", i) }
+	//consoleWriter.FormatFieldValue = func(i interface{}) string { return strings.ToUpper(fmt.Sprintf("%s", i)) }
+
+	// 设置 timestamp 的格式为 yyyy-MM-dd HH:mm:ss
+	//consoleWriter.FormatTimestamp = func(i time.Time) string {
+	//	return i.Format("2006-01-02 15:04:05")
+	//}
+	logger = log.Output(consoleWriter).With().Timestamp().Logger()
+
+	//logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout}).With().Timestamp().Logger()
 	//logger = log.With().Timestamp().CallerWithSkipFrameCount(3).Stack().Logger()
 }
 
@@ -68,4 +84,14 @@ func Log(message string, args ...interface{}) {
 	} else {
 		logger.Info().Msgf(message, args...)
 	}
+}
+
+func main() {
+	output := zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339}
+	output.FormatLevel = func(i interface{}) string { return strings.ToUpper(fmt.Sprintf("| %-6s|", i)) }
+	output.FormatMessage = func(i interface{}) string { return fmt.Sprintf("***%s****", i) }
+	output.FormatFieldName = func(i interface{}) string { return fmt.Sprintf("%s:", i) }
+	output.FormatFieldValue = func(i interface{}) string { return strings.ToUpper(fmt.Sprintf("%s", i)) }
+	logger := log.Output(output).With().Timestamp().Logger()
+	logger.Info().Str("foo", "bar").Msg("hello world")
 }
